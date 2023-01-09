83da1c265fad6525f5850294358f8a39a44d7b39
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2023-01-08 23:21:02 -0300

    config: configured database access; added transform interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'],
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_SERVER,
+      //port: parseInt(process.env.DB_PORT, 10),
+      port: 5433,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_SCHEMA,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
